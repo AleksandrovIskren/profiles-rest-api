8e843ec7896398d6aeec821a40ba09177e956d79
unknown <sk94bg@gmail.com>
2021-01-06 20:58:28 +0200

    Added APIViews

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 """
 APIViews are used when:
@@ -11,6 +14,7 @@ APIViews are used when:
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -23,18 +27,27 @@ class HelloApiView(APIView):
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
 
 
-    def post(self, request, format=None):
-        """Create a new item"""
-        return Response({'new': 'item'})
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
 
-    def put(self, request, format=None):
-        """Update an Item"""
-        return Response({'updated': 'item'})
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
 
-    def patch(self, request, format=None):
-        """Partially update an item"""
-        return Response({'partially_updated': 'item'})
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
 
-    def delete(self, request, format=None):
-        """Delete an item"""
-        return Response({'deleted': 'item'})
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
