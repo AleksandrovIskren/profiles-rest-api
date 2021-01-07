877601d4089266225e5862cbc5bc0e56bd20358f
unknown <sk94bg@gmail.com>
2021-01-07 16:39:45 +0200

    Added profile authentication

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,10 +6,11 @@ from profiles_api import views
 """This is the way to register a URL for a ViewSet"""
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
-router.register('profile-viewset', views.UserProfileViewSet)
+router.register('profile', views.UserProfileViewSet)
 
 """This is the way to register a URL for a APIView"""
 urlpatterns = [
                 path('hello-view/', views.HelloApiView.as_view()),
+                path('login/', views.UserLoginApiView.as_view()),
                 path('', include(router.urls)) # This part is for the viewset
             ]
